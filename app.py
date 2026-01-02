f5c98a5ecd4b6679e8db9c18eece12653c8a179a
alex100588 <alexandru_ionasc@yahoo.com>
2026-01-02 15:31:49 +0200

    variables

--- app.py
+++
@@ -1,9 +0,0 @@
-print('hello world')
-print("*" * 10)
-
-print("hello world")
-
-x = 1
-y = 2
-
-print(x)
---
+++ variables/app.py
@@ -0,0 +1,8 @@
+student_count = 1000
+rating = 4.99
+is_published = False
+a_string = "My name is"
+total = 10 + student_count
+print(student_count, rating, is_published)
+print(a_string)
+print(total)
