c02c0900c5bf2d8a6628139a5f83a078c7e9ad94
alex100588 <alexandru_ionasc@yahoo.com>
2026-01-04 10:01:01 +0200

    strings

--- variables/app.py
+++ variables/app.py
@@ -2,7 +2,7 @@ student_count = 1000
 rating = 4.99
 is_published = False
 a_string = "My name is"
-total = 10 + student_count
+total = 10 + rating
 print(student_count, rating, is_published)
 print(a_string)
 print(total)
