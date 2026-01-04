bf8371495c2ffee531676af81866fd4fed74f9c6
alex100588 <alexandru_ionasc@yahoo.com>
2026-01-04 10:00:55 +0200

    escape-sequence

---
+++ escape-sequence/app.py
@@ -0,0 +1,9 @@
+course = "Python \"Programming"
+course1 = "Python \'Programming"
+course2 = "Python \\Programming"
+course3 = "Python \nProgramming"
+
+print(course)
+print(course1)
+print(course2)
+print(course3)
